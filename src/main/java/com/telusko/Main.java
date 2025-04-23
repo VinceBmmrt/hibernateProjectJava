7143117f97e34e5cde4b42c996dac18762c40d47
VinceBmmrt <vincebommert75@gmail.com>
2025-04-23 16:53:28 +0200

    Hibernate : usage of HQL for fetching with filter and specific properties

--- src/main/java/com/telusko/Main.java
+++ src/main/java/com/telusko/Main.java
@@ -1,5 +1,6 @@
 package com.telusko;
 
+import jakarta.persistence.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -7,70 +8,30 @@ import org.hibernate.cfg.Configuration;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        Laptop l1 = new Laptop();
-        l1.setLid(1);
-        l1.setBrand("Asus");
-        l1.setModel("Rog");
-        l1.setRam(16);
-
-        Laptop l2 = new Laptop();
-        l2.setLid(2);
-        l2.setBrand("Dell");
-        l2.setModel("Premium");
-        l2.setRam(32);
-
-        Laptop l3 = new Laptop();
-        l3.setLid(3);
-        l3.setBrand("Apple");
-        l3.setModel("Mac");
-        l3.setRam(32);
-
-        Alien a1 = new Alien();
-        a1.setAid(101);
-        a1.setAname("chewbaka");
-        a1.setTech("Java");
-
-
-        Alien a2 = new Alien();
-        a2.setAid(102);
-        a2.setAname("ET");
-        a2.setTech("Next");
-
-
-        a1.setLaptops(Arrays.asList(l1, l2));
-        a2.setLaptops(Arrays.asList(l3));
-
 
         SessionFactory sf = new Configuration()
                 .configure()
-                .addAnnotatedClass(com.telusko.Alien.class)
                 .addAnnotatedClass(com.telusko.Laptop.class)
-                .buildSessionFactory();  //cfg.buildSessionFactory();
+                .buildSessionFactory();
 
         Session session = sf.openSession();
 
-        Transaction transaction = session.beginTransaction();
+        // select * from laptop where ram = 32 => SQL
+        // from Laptop where ram = 32 => HQL
+        Query query = session.createQuery("from Laptop where ram = 32");
+        List<Laptop> laptops = query.getResultList();
 
-        session.persist(l1);
-        session.persist(l2);
-        session.persist(l3);
-
-        session.persist(a1);
-        session.persist(a2);
+        // Laptop l1 = session.get(Laptop.class, 3);
 
 
-        transaction.commit();
-
+        System.out.println(laptops);
 
         session.close();
-
-        Session session1 = sf.openSession();
-        Alien a5 = session1.get(Alien.class, 101);
-        //System.out.println(a5);
 
         sf.close();
 
